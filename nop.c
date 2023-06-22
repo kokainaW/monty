cbf5df45a7bd55b0c7c778503f64fb1a05dfae7a
kokainaW <williamkhoko@yahoo.com>
2023-06-22 11:58:00 +0000

    monty

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- it is nothing
+  *@head: the stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ pstr.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * f_pstr - will print the string starting at the top of the stack,
+ * followed by a new
+ * @head: Stack head
+ * @counter: Line_number
+ * Return: no return
+*/
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
